bfba2db5ca5be0eecfb6e645cf3fc019862e31f5
Håkan Sagvik <hakan.sagvik@gmail.com>
2018-02-21 21:25:16 +0100

    Add project files.

---
+++ BlankEpiSite/Controllers/StartPageController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using BlankEpiSite.Models.Pages;
+using EPiServer.Web.Mvc;
+
+namespace BlankEpiSite.Controllers
+{
+    public class StartPageController : PageController<StartPage>
+    {
+        public ActionResult Index(StartPage currentPage)
+        {
+            /* Implementation of action. You can create your own view model class that you pass to the view or
+             * you can pass the page type for simpler templates */
+
+            return View(currentPage);
+        }
+    }
+}
---
+++ BlankEpiSite/Models/Pages/StartPage.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace BlankEpiSite.Models.Pages
+{
+    [ContentType(DisplayName = "StartPage", GUID = "113bc425-8bcb-4649-b523-caf5f40926be", Description = "")]
+    public class StartPage : PageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description =
+                "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
